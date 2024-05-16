d7fb6c9ffadc47e7f7dd8643c03cbbaa907e3964
amino0422 <sekainoowari.nakajin@gmail.com>
2024-05-17 00:58:30 +0900

    エラーの修正２

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :destroy, :edit, :update]
-  before_action :move_to_index
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @events = Event.includes(:user)
